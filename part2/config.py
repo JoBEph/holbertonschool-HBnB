3fcf1bfea50c74375c2f67f267fa46fe70c9d8b9
root <root@JordanPro.>
2025-02-24 11:32:53 +0100

    Update based on pycodestyle

--- part2/config.py
+++ part2/config.py
@@ -1,23 +1,28 @@
 import os
 
+
 class Config:
     """Base configuration for the secret key and debug"""
 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "secret_key"
     Debug = False
 
+
 class DevelopmentConfig(Config):
     """Configuration for the development environment"""
     DEBUG = True
+
 
 class TestingConfig(Config):
     """Testing configuration for the environment"""
     TESTING = True
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configuration for the production environment"""
     DEBUG = False
+
 
 config = {
     "development": DevelopmentConfig,
