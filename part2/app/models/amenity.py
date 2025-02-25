0a1f0cac865bb09640b70ffee1ac5e4c8dcc3c09
Maxime521 <9842@holbertonstudents.com>
2025-02-25 13:31:51 +0100

    I just continued the amenity

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
 Base_class = __import__("base_class").Baseclass
+
 
 class Amenity(Base_class):
     """
     Amenity class that defines attributes and methods for amenities.
-    Inherits from Base_class which provides common attributes like id, created_at, and updated_at.
+    Inherits from Base_class which provides common attributes.
     """
     name = ""
 
