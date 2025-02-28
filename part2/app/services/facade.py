a321ccdb235a92c5a85cf0e14d60198ba6bd2e1a
Maxime521 <9842@holbertonstudents.com>
2025-02-28 17:43:57 +0100

    modified the code

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,9 +1,10 @@
-from app.models.user import User, Review
+from app.models.user import User, Review, Amenity
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     # User methods
     def create_user(self, user_data):
@@ -49,3 +50,24 @@ class HBnBFacade:
         review = self.review_repo.get(review_id)
         self.review_repo.delete(review)
         return review
+
+    # Amenity methods
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
+
+    def delete_amenity(self, amenity_id):
+        amenity = self.amenity_repo.get(amenity_id)
+        self.amenity_repo.delete(amenity)
+        return amenity
