170c73bd00739fb8b0cfcf35b6bb974c2e34553a
JoBEph <jordan.baroux31@gmail.com>
2025-03-12 10:42:34 +0100

    update code for exigence from task 7

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from app.models.base_class import Baseclass
 from app.models.place import Place
 from app.models.user import User
