95f5e621e6a5c58b5bca48bf6a675f308e7848f6
JoBEph <jordan.baroux31@gmail.com>
2025-03-12 11:07:43 +0100

    add BaseModel to the BaseClass

--- part3/app/models/base_class.py
+++ part3/app/models/base_class.py
@@ -3,8 +3,16 @@
 This file provides attribute and methods accessible 
 bay other class 
 """
+from app import db
 import uuid
 from datetime import datetime
+
+class BaseModel(db.Model):
+     __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+
+id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+created_at = db.Column(db.DateTime, default=datetime.utcnow)
+updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 class Baseclass:
     def __init__(self):
