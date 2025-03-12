b84a58307f52bcd5aba39edfcb68968fde63b153
JoBEph <jordan.baroux31@gmail.com>
2025-03-12 11:07:05 +0100

    back up for relationship

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from app.models.base_class import Baseclass
 from app.models.place import Place
 from app.models.user import User
@@ -11,8 +12,11 @@ class Review(Baseclass):
     id = Column(Integer, primary_key=True)
     text = Column(String)
     rating = Column(Integer)
-    place_id = Column(Integer)
-    user_id = Column(Integer)
+    place_id = Column(Integer, ForeignKey('places.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    place = relationship('Place', backref='reviews')
+    user = relationship('User', backref='reviews')
 
     def __init__(self, text, rating, place_id, user_id):
         super().__init__()
@@ -27,6 +31,10 @@ class Review(Baseclass):
             raise ValueError('Error: Text is empty.')
         if not (1 <= self.rating <= 5):
             raise ValueError('Error: Choose between 1 and 5')
+        if Place.query.get(self.place_id) is None:
+            raise ValueError('Error: Place does not exist')
+        if User.query.get(self.user_id) is None:
+            raise ValueError('Error: User does not exist')
 
     def update(self, text=None, rating=None, place_id=None, user_id=None):
         if text is not None:
@@ -43,5 +51,5 @@ class Review(Baseclass):
     def __str__(self):
         return (
             f'Review: (id={self.id}, text={self.text}, rating={self.rating}, '
-            f'place_id={self.place_id}, user_id={self.user_id})'
+            f'place={self.place}, user={self.user})'
         )
