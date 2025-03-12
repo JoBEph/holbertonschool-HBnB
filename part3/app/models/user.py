7f922192f530e628266ac8db9d0d87a8b238def6
JoBEph <jordan.baroux31@gmail.com>
2025-03-12 11:52:51 +0100

    fix indetation

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -8,13 +8,13 @@ from .base_class import Baseclass, BaseModel
 
 
 class User(BaseModel):
-     __tablename__ = 'users'
+    __tablename__ = 'users'
 
-     first_name = db.Column(db.String(50), nullable=False)
-     last_name = db.Column(db.String(50), nullable=False)
-     email = db.Column(db.String(120), nullable=False, unique=True)
-     password = db.Column(db.String(128), nullable=False)
-     is_admin = db.Column(db.Boolean, default=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def hash_password(self, password):
         """Hash the password before storing it."""
