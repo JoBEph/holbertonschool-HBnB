db241cc2248f5b92e91111467c570bf0939dc67b
JoBEph <jordan.baroux31@gmail.com>
2025-03-12 10:18:28 +0100

    update review for sql alchemy

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,14 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from app.models.base_class import Baseclass
 from app.models.place import Place
 from app.models.user import User
 
+
 class Review(Baseclass):
+
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    rating = Column(Integer)
+    place_id = Column(Integer, ForeignKey('places.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    place = relationship('Place', backref='reviews')
+    user = relationship('User', backref='reviews')
+
     def __init__(self, text, rating, place_id, user_id):
         super().__init__()
         self.text = text
         self.rating = rating
-        self.place = place_id
-        self.user = user_id
+        self.place_id = place_id
+        self.user_id = user_id
         self.validate()
 
     def validate(self):
@@ -16,20 +31,20 @@ class Review(Baseclass):
             raise ValueError('Error: Text is empty.')
         if not (1 <= self.rating <= 5):
             raise ValueError('Error: Choose between 1 and 5')
-        if self.place is None:
+        if Place.query.get(self.place_id) is None:
             raise ValueError('Error: Place does not exist')
-        if self.user is None:
+        if User.query.get(self.user_id) is None:
             raise ValueError('Error: User does not exist')
 
     def update(self, text=None, rating=None, place_id=None, user_id=None):
         if text is not None:
-             self.text = text
+            self.text = text
         if rating is not None:
             self.rating = rating
         if place_id is not None:
-            self.place = place_id
+            self.place_id = place_id
         if user_id is not None:
-            self.user = user_id
+            self.user_id = user_id
         self.updated_at = Baseclass.datetime.now()
         self.validate()
 
